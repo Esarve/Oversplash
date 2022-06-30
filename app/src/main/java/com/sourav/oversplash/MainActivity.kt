2f9f983a27e6b0072b1ac367422459995c152cad
Sourav Das <esarve.srv@gmail.com>
2022-06-30 14:39:46 +0600

    Initial commit

---
+++ app/src/main/java/com/sourav/oversplash/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sourav.oversplash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
