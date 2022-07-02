8df6d491dfd9427ffc8cb988e89f2edb295f46d8
Sourav Das <esarve.srv@gmail.com>
2022-07-02 12:51:04 +0600

    - added timber for logs

--- app/src/main/java/com/sourav/oversplash/Oversplash.kt
+++ app/src/main/java/com/sourav/oversplash/Oversplash.kt
@@ -1,12 +1,16 @@
 package com.sourav.oversplash
 
 import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+
 
 class Oversplash: Application() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        if (BuildConfig.DEBUG) plant(Timber.DebugTree())
     }
 
     companion object {
